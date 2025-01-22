2518f041f23f846033539c93bb4ca8fc01771847
louol <34587874+louol@users.noreply.github.com>
2025-01-22 12:57:06 +0800

    Update module_16_1.py
    str

--- module_16_1.py
+++ module_16_1.py
@@ -3,19 +3,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/')
-def read_main() -> dict:
+def read_main() -> str:
     return {'message': 'Главная страница'}
 
 @app.get('/user/admin')
-def read_admin() -> dict:
+def read_admin() -> str:
     return {'message': 'Вы вошли как администратор'}
 
 @app.get('/user/{user_id}')
-def read_user(user_id: int) -> dict:
+def read_user(user_id: int) -> str:
     return {'message': f'Вы вошли как пользователь №  {user_id}'}
 
 @app.get('/user')
-def read_user_info(username: str = '123', age: int = 56) -> dict:
+def read_user_info(username: str = '123', age: int = 56) -> str:
     return {'message': f'Информация о пользователе. Имя:  {username}, Возраст: {age}'}
 
 
